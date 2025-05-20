a7c56ff797083a8f77d578d4305e0fa93f53c4a7
nannaluie <nanni20@student.sdu.dk>
2025-05-20 12:30:39 +0200

    Changed files

---
+++ Core/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module Core {
+    requires Common;
+
+    requires CommonBullet;    
+    requires javafx.graphics;   
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+    exports dk.sdu.mmmi.cbse.main;
+    opens dk.sdu.mmmi.cbse.main to javafx.graphics,spring.core;
+    uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+    uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+    uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+}
+
+
